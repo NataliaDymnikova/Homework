4e17587c8dbb0f337928d784dfbd4ae9f11e7296
Natalia.Dymnikova <natalia.dymnikova@gmail.com>
2015-02-16 22:15:05 +0300

    Fibbonacci numbers

---
+++ 4semester/1. 11.02.15/2Fibbonacci/2Fibbonacci/Program.fs
@@ -0,0 +1,13 @@
+﻿// Fibbonacci numbers.
+[<EntryPoint>]
+let main argv = 
+    System.Console.WriteLine("Put number:")
+    let number : int = int (System.Console.ReadLine())
+
+    let rec Fibbonacci num =
+        if num <= 1
+        then 1
+        else Fibbonacci (num - 2) + Fibbonacci (num - 1)
+
+    System.Console.WriteLine("Fibbonacci: {0}", Fibbonacci number)
+    0
