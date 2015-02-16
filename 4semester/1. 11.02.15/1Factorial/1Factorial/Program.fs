ef43adcf3fd1333d014537c4757c942b3b61e2d7
Natalia.Dymnikova <natalia.dymnikova@gmail.com>
2015-02-16 22:06:21 +0300

    Factorial done

---
+++ 4semester/1. 11.02.15/1Factorial/1Factorial/Program.fs
@@ -0,0 +1,15 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+// Дополнительную справку см. в проекте "Учебник по F#".
+
+[<EntryPoint>]
+let main argv = 
+    System.Console.WriteLine("Put a number:")
+    let string = System.Console.ReadLine()
+    let number : int = int string
+
+    let rec factorial n =
+        if n = 1 
+        then 1 
+        else n * factorial (n - 1) 
+    System.Console.WriteLine("Factorial: {0}", factorial number)
+    0 // возвращение целочисленного кода выхода
