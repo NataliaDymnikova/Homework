11615f965d6efb632144ed172281dc480b66a642
NataliaDymnikova <natalia.dymnikova@gmail.com>
2015-02-23 15:15:06 +0300

    1ConvertList

---
+++ 4semester/2. 20.02.15/1ConvertList/1ConvertList/Program.fs
@@ -0,0 +1,11 @@
+﻿/// Convert list and print to console.
+let ConvertList =
+    let listFirst :list<int> = [1; 2; 1; 3]
+
+    let rec func previousList = 
+        match previousList with
+        | head::tail -> List.append (func tail) [head] 
+        | [] -> []
+
+    List.iter (fun x -> printfn "%d " x) (func listFirst)            
+    
