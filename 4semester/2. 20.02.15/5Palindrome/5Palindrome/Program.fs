63d6fa255ee461ceaa2ab030df3a6e88faa04d00
NataliaDymnikova <natalia.dymnikova@gmail.com>
2015-02-23 21:56:39 +0300

    Make it faster

--- 4semester/2. 20.02.15/5Palindrome/5Palindrome/Program.fs
+++ 4semester/2. 20.02.15/5Palindrome/5Palindrome/Program.fs
@@ -1,14 +1,14 @@
-﻿/// Return true - if list is palindrome. Make first = [].
-let rec isPalindrome list first = 
-    if List.length list = 0
-        then true
-    elif List.length list = 1 && List.length first = 1 
-        then first.Head = list.Head
-    elif list.Length = 1
-        then (first.Head = list.Head) && (isPalindrome first.Tail.Tail [first.Tail.Head])
-    else
-        isPalindrome list.Tail (List.append first [list.Head])
+﻿/// Return true - if list is palindrome.
+let isPalindrome str =
+    let rec convertList list result = 
+        match list with
+        | head::tail -> convertList tail (head::result)
+        | [] -> result
 
+    let list = List.ofArray ((string str).ToCharArray())
+    let convert = convertList list []
+    convert = list
+    
 printfn "Put string:"
-let list = List.ofArray (System.Console.ReadLine().ToCharArray())
-printfn "This string is palindrome - %b" (isPalindrome list [])
+let str = System.Console.ReadLine()
+printfn "This string is palindrome - %b" (isPalindrome str)
