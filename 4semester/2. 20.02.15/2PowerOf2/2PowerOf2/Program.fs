7a084306aa6514c1d3f069090e15e83325dec586
NataliaDymnikova <natalia.dymnikova@gmail.com>
2015-02-23 16:32:01 +0300

    2PowerOf2

---
+++ 4semester/2. 20.02.15/2PowerOf2/2PowerOf2/Program.fs
@@ -0,0 +1,17 @@
+﻿let number = 5
+
+/// Return list
+let rec powerOf2 count previousPower list = 
+    if count = 1
+        then List.append list [previousPower]
+    else
+        powerOf2 (count - 1) (previousPower * 2) (List.append list [previousPower])
+
+/// result^number
+let rec power number result = 
+    if number >= 1
+        then power (number - 1) (result * 2)
+    else
+        result
+
+List.iter (fun x -> printf "%d " x) (powerOf2 5 (power number 1) [])
