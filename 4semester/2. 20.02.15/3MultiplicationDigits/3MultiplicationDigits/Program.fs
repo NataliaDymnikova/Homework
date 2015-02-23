685a09186dfd03e022e395af35457d692182f11f
NataliaDymnikova <natalia.dymnikova@gmail.com>
2015-02-23 16:49:50 +0300

    3MultiplicationDigits

---
+++ 4semester/2. 20.02.15/3MultiplicationDigits/3MultiplicationDigits/Program.fs
@@ -0,0 +1,10 @@
+﻿/// Return multiplication of digits in number.
+let rec multiplication number = 
+    if number > 9
+        then (number % 10) * (multiplication (number / 10))
+    else
+        number
+
+printfn "Put big number:"
+let number = int <| System.Console.ReadLine()
+multiplication number |> printfn "Multiplication of digits %d"
