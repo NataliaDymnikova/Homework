949474a1ef239ed79766bc7c77d9850431947aec
NataliaDymnikova <natalia.dymnikova@gmail.com>
2015-02-23 17:14:58 +0300

    4. Position of number in list

---
+++ 4semester/2. 20.02.15/4AmountOfNumber/4AmountOfNumber/Program.fs
@@ -0,0 +1,15 @@
+﻿/// Return position of nimber in list.
+let rec positionOfNumber list number amount = 
+    if List.length list = 0
+        then -1
+    elif List.head list = number
+        then amount
+    else
+        positionOfNumber (List.tail list) number (amount + 1)
+
+printfn "Put list:"
+let list = List.map (fun x -> int <| x) (List.ofArray (System.Console.ReadLine().Split(' ')))
+printfn "Put number:"
+let number = int <| System.Console.ReadLine()
+
+printfn "Amount of %d: %d" number (positionOfNumber list number 0)
