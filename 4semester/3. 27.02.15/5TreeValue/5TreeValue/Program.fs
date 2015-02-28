8a683d69c9decd3369cd5216f74620902a20164a
NataliaDymnikova <natalia.dymnikova@gmail.com>
2015-02-28 11:45:31 +0300

    5TreeValue

---
+++ 4semester/3. 27.02.15/5TreeValue/5TreeValue/Program.fs
@@ -0,0 +1,18 @@
+﻿type Sign = char
+type Number = int
+type ParseTree =
+    | Node of Number
+    | ParseTree of Sign * ParseTree * ParseTree
+
+/// Return value of parse tree.
+let rec value tree = 
+    match tree with
+    | ParseTree('+', left, right) -> (value left) + (value right)
+    | ParseTree('-', left, right) -> (value left) - (value right)
+    | ParseTree('*', left, right) -> (value left) * (value right)
+    | ParseTree('/', left, right) -> (value left) / (value right)
+    | Node x -> x
+    | _ -> 0
+
+let tree = ParseTree('+', ParseTree('-', Node 5, Node 1), ParseTree('*', Node 1, Node 3))
+printfn "Value: %d" (value tree)
