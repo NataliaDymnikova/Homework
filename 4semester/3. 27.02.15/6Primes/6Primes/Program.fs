0f91f54361367294d23e5cb674697338e0b6c5e5
NataliaDymnikova <natalia.dymnikova@gmail.com>
2015-02-28 22:35:35 +0300

    6Primes

---
+++ 4semester/3. 27.02.15/6Primes/6Primes/Program.fs
@@ -0,0 +1,20 @@
+﻿/// Return all prime numbers
+let seqAllPrimes = 
+    let rec seqPrime currentNumber = seq {
+        let rec isPrime number currentNumber = 
+            if number * number <= currentNumber then
+                if currentNumber % number = 0 then
+                    false
+                else
+                    isPrime (number + 1) currentNumber
+            else
+                true
+
+        if (isPrime 2 currentNumber) then
+            yield currentNumber
+        yield! seqPrime (currentNumber + 1)
+    }
+    seqPrime 2
+
+let somePrimes = Seq.truncate 100 seqAllPrimes
+Seq.iter (fun x -> printf "%d " x) somePrimes
