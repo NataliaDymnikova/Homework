12e85cda95dbffa9be470690221f865ef0770ba1
NataliaDymnikova <natalia.dymnikova@gmail.com>
2015-02-28 11:04:49 +0300

    3EvenNumber

---
+++ 4semester/3. 27.02.15/3EvenNumber/3EvenNumber/Program.fs
@@ -0,0 +1,15 @@
+﻿let numberOfEvenNumberByMap list = 
+    List.length (List.concat (List.map (fun x -> if x % 2 = 0 then [x] else []) list))
+
+let numberOfEvenNumberByFilter list = 
+    List.length (List.filter (fun x -> x % 2 = 0) list)
+    
+let numberOfEvenNumberByFold list = 
+    List.fold (fun acc x -> if x % 2 = 0 then (acc + 1) else acc) 0 list
+
+printfn "Put list:"
+let list = List.map (fun x -> int <| x) (List.ofArray (System.Console.ReadLine().Split(' ')))
+printfn "Number of even numbers "
+printfn "by map: %d" (numberOfEvenNumberByMap list)
+printfn "by filter: %d" (numberOfEvenNumberByFilter list)
+printfn "by fold: %d" (numberOfEvenNumberByFold list)
