d9a3ec79d77a21f2ca2185d5b1baf658b3d36165
NataliaDymnikova <natalia.dymnikova@gmail.com>
2015-02-28 11:28:50 +0300

    4TreeHeight

---
+++ 4semester/3. 27.02.15/4TreeHeight/4TreeHeight/Program.fs
@@ -0,0 +1,14 @@
+﻿type Tree<'a> = 
+    | Tree of 'a * Tree<'a> * Tree<'a>
+    | Node of 'a
+
+/// Return height of Tree.
+let heightOfTree tree =
+    let rec height tree result =
+        match tree with
+        | Tree(_, left, right) -> max (height left (result + 1)) (height right (result + 1))
+        | Node _ -> result
+    height tree 1
+
+let tree = Tree(1, Tree(2, Tree.Node(3), Tree.Node(4)), Tree.Node(5))
+printfn "Height of tree: %d" (heightOfTree tree)
