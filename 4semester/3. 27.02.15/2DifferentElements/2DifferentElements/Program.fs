25cf0453109d08a94e7eb84b781f1644247feab3
NataliaDymnikova <natalia.dymnikova@gmail.com>
2015-02-28 10:30:43 +0300

    2DifferentElemenets

---
+++ 4semester/3. 27.02.15/2DifferentElements/2DifferentElements/Program.fs
@@ -0,0 +1,21 @@
+﻿/// Return true if all elements are different, false - otherwise.
+let isDifferentAllElements list = 
+    let rec isDifferentThis list currentElement = 
+        match list with
+        | head::tail -> if currentElement <> head then 
+                            isDifferentThis tail currentElement
+                        else
+                            false
+        | [] -> true
+
+    let rec isDifferentAll list = 
+        match list with
+        | head::tail -> if not (isDifferentThis tail head) then false
+                        else isDifferentAll tail
+        | [] -> true
+
+    isDifferentAll list
+
+printfn "Put list:"
+let list = List.map (fun x -> int <| x) (List.ofArray (System.Console.ReadLine().Split(' ')))
+printfn "All elements are different is %b" (isDifferentAllElements list)
