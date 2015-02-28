33b632b818eb733045971aed83d119e7d593d3bf
NataliaDymnikova <natalia.dymnikova@gmail.com>
2015-02-28 10:14:57 +0300

    1PositionOfMaxSum

---
+++ 4semester/3. 27.02.15/1PositionOfMaxSum/1PositionOfMaxSum/Program.fs
@@ -0,0 +1,15 @@
+﻿/// Return index of max sum two adjacent elements in listю
+let  positionOfMaxSum list = 
+    let rec position list currentIndex resultIndex resultSum = 
+        match list with
+        | head::tail when tail <> [] -> if (head + tail.Head) > resultSum then
+                                            position tail (currentIndex + 1) currentIndex (head + tail.Head)
+                                        else
+                                            position tail (currentIndex + 1) resultIndex resultSum
+        | _ -> resultIndex
+
+    position list 1 -1 -1
+
+printfn "Put list:"
+let list = List.map (fun x -> int <| x) (List.ofArray (System.Console.ReadLine().Split(' ')))
+printfn "Position of max sum: %d" (positionOfMaxSum list)
