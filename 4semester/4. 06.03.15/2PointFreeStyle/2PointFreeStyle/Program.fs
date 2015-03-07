fbfad857f8bfcd1f8f97f48c11828947909f3814
NataliaDymnikova <natalia.dymnikova@gmail.com>
2015-03-07 22:03:46 +0300

    2PointFreeStyle

---
+++ 4semester/4. 06.03.15/2PointFreeStyle/2PointFreeStyle/Program.fs
@@ -0,0 +1,17 @@
+﻿let func x l = List.map (fun y -> y * x) l
+let func2 x : List<int> -> List<int> = List.map (fun y -> y * x)
+let func3 : int -> List<int> -> List<int> = (fun y -> (*) y) >> List.map
+let func4 : int -> List<int> -> List<int> = ((*)) >> List.map 
+
+printfn "Put list"
+let list = List.map (int) (List.ofArray (System.Console.ReadLine().Split(' ')))
+
+List.iter (printf "%d ") (func 2 list)
+printfn ""
+List.iter (printf "%d ") (func2 2 list)
+printfn ""
+List.iter (printf "%d ") (func3 2 list)
+printfn ""
+List.iter (printf "%d ") (func4 2 list)
+printfn ""
+
