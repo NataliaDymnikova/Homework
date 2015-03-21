d7a221715251316e69aa010814dfcc8bbf677dcd
NataliaDymnikova <natalia.dymnikova@gmail.com>
2015-03-21 23:52:53 +0300

    Add search and good work with file

--- 4semester/4. 06.03.15/3Phones/3Phones/Program.fs
+++ 4semester/4. 06.03.15/3Phones/3Phones/Program.fs
@@ -2,50 +2,53 @@
 type Name = string
 type Phones = 
     | Contact of Name * Number
-    | Contactes of Name * Number * Phones
+    | Contacts of Name * Number * Phones
     | Empty
+type ComparePhones<'a> = (Phones -> 'a -> bool) 
 
 /// Add new contact to phones.
 let add name number phones = 
-    Contactes(name, number, phones)
+    Contacts(name, number, phones)
+
+/// Search
+let rec search (phones : Phones) item compare =
+        match phones with
+        | Empty -> ("",-1)
+        | Contact (cName, cNumber) ->
+            if compare (Contact(cName,cNumber)) item then
+                (cName, cNumber)
+            else
+                ("",-1)
+        | Contacts (cName, cNumber, cPhones) ->
+            if compare (Contact(cName,cNumber)) item then
+                (cName, cNumber)
+            else
+                search cPhones item compare
+
 
 /// Search by name.
 let rec searchByName (phones: Phones) name = 
-    match phones with
-    | Contact (cName, cNumber) -> 
-        if name = cName then
-            cNumber
-        else
-            -1
-    | Contactes(cName, cNumber, cPhones) -> 
-        if name = cName then
-            cNumber
-        else
-            searchByName cPhones name
-    | Empty -> -1
+    let compare phones name = 
+            match phones with
+            | Contact (cName, cNumber) -> cName = name
+            | _ -> false
+    snd(search phones name compare)
 
 /// Search by number.
 let rec searchByNumber (phones: Phones) number = 
-    match phones with
-    | Contact (cName, cNumber) -> 
-        if number = cNumber then
-            cName
-        else
-            "Not Found"
-    | Contactes(cName, cNumber, cPhones) -> 
-        if number = cNumber then
-            cName
-        else
-            searchByNumber cPhones number
-    | Empty -> "Empty"
+    let compare phones num = 
+        match phones with
+        | Contact (cName, cNumber) -> cNumber = num
+        | _ -> false
+    fst(search phones number compare)
 
 /// Convert phones to string.
 let phonesToString phones = 
     let rec recPhonesToString phones (str: List<string>) = 
         match phones with
-        | Contact (name, number) -> ((string <| name) + " " + number.ToString())::str
-        | Contactes (name, number, cPhones) -> 
-            recPhonesToString cPhones (((string <| name) + " " + number.ToString())::str)
+        | Contact (name, number) -> ((string name) + " " + number.ToString())::str
+        | Contacts (name, number, cPhones) -> 
+            recPhonesToString cPhones (((string name) + " " + number.ToString())::str)
         | Empty -> str
 
     recPhonesToString phones List<string>.Empty
@@ -56,7 +59,7 @@ let stringsToPhones (contactes: List<string>) =
         match contactes with
         | head::tail -> 
             let str = List.ofArray (head.Split(' '))
-            recStringToPhones tail (Contactes(str.Head, (int <| str.Tail.Head), phones))
+            recStringToPhones tail (Contacts(str.Head, (int <| str.Tail.Head), phones))
         | [] -> phones
     recStringToPhones contactes Phones.Empty
 
@@ -66,8 +69,11 @@ let writeToFile fileName (phones: Phones) =
 
 /// Read phones from file.
 let readFromFile fileName = 
-    let contactes : List<string> = List.ofArray (System.IO.File.ReadAllLines(fileName))
-    stringsToPhones contactes
+    if  System.IO.File.Exists fileName then
+        let contactes : List<string> = List.ofArray (System.IO.File.ReadAllLines(fileName))
+        stringsToPhones contactes
+    else
+        Empty
 
 /// Add, search and other in console.
 let rec workWithUser phones=
@@ -105,6 +111,6 @@ let rec workWithUser phones=
         workWithUser (readFromFile "phones.txt")
     | _ -> workWithUser phones
 
-let phones = Contactes("aaa", 111, Contactes("bbb", 222, Contact("ccc", 333)))
+let phones = Contacts("aaa", 111, Contacts("bbb", 222, Contact("ccc", 333)))
 let telephones = workWithUser phones
 
