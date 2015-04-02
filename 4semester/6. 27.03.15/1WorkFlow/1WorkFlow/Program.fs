9709d699cd9abe91e62183415a2e14c338c6e415
NataliaDymnikova <natalia.dymnikova@gmail.com>
2015-04-02 23:11:08 +0300

    1WorkFlow

---
+++ 4semester/6. 27.03.15/1WorkFlow/1WorkFlow/Program.fs
@@ -0,0 +1,14 @@
+﻿type rounding(precision : int) =
+    member this.Bind((x : float), (rest : float -> float)) =
+        rest x
+    member this.Return (x : float) = 
+        System.Math.Round (x, precision)
+
+let example =
+    rounding 3 {
+        let! a = 2.0 / 12.0
+        let! b = 3.5
+        return a / b
+    }
+
+printfn "%A" example
