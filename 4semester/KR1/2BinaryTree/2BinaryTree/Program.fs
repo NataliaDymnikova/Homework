dfcc8dc1a2534109524c76b2c92c5d9668ab453a
NataliaDymnikova <natalia.dymnikova@gmail.com>
2015-04-03 11:40:23 +0300

    2BinaryTree

---
+++ 4semester/KR1/2BinaryTree/2BinaryTree/Program.fs
@@ -0,0 +1,21 @@
+﻿/// Binary tree
+type Tree<'a> = 
+    | Tree of 'a * Tree<'a> * Tree<'a>
+    | Node of 'a
+
+/// Filter of tree
+let rec filter (condition : 'a -> bool) (tree : Tree<'a>)= 
+    match tree with
+    | Node node -> 
+        if condition node then
+            [node]
+        else
+            []
+    | Tree (node, left, right) ->
+        if condition node then
+            node :: (filter condition left) @ (filter condition right)
+        else
+            (filter condition left) @ (filter condition right)
+
+let tree = Tree(1, Tree(2, Tree.Node(3), Tree.Node(4)), Tree.Node(5))
+printfn "%A" (filter (fun x -> x % 2 = 0) tree)
