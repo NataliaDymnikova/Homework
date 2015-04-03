2a62e22b3f6cc676376b6acb1d734fd0ab67ee2d
NataliaDymnikova <natalia.dymnikova@gmail.com>
2015-04-03 11:27:30 +0300

    1SumFib

---
+++ 4semester/KR1/1SumFib/1SumFib/Program.fs
@@ -0,0 +1,19 @@
+﻿/// Return Fibbonacci
+let rec Fibbonacci num =
+    if num <= 1
+    then 1
+    else Fibbonacci (num - 2) + Fibbonacci (num - 1)
+
+/// Return sum of first million Fibbonacci numbers
+let SumFib =
+    let rec recSumFib num =
+        let numFib = Fibbonacci num
+        if numFib > 1000000 then
+            0
+        elif numFib % 2 = 0 then
+            recSumFib (num + 1) + numFib
+        else
+        recSumFib (num + 1)
+    recSumFib 1
+
+printfn "%d" SumFib
