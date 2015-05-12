70c0ea331448381983ad0c7b802b4a3f3171dcf5
NataliaDymnikova <natalia.dymnikova@gmail.com>
2015-05-12 20:48:26 +0300

    Small corrections.

--- 4semester/7. 10.04.15/1/1/Program.fs
+++ 4semester/7. 10.04.15/1/1/Program.fs
@@ -1,39 +1,48 @@
 ﻿open System.ComponentModel
 
 let mutable result = 0
-
-type PlusOneEvet (array : int list, start) = 
+let array = Array.create 1000000 1
+//let array = [for x in 1..1000000 -> 1]
+    
+type PlusOneEvet (start) = 
     let number = 10000
     let worker = new BackgroundWorker()
     
     let completed = new Event<_>()
     let cancelled = new Event<_>()
+    let error = new Event<_>()
+    let progress = new Event<_>()
     
-    do worker.RunWorkerCompleted.Add(fun args ->
-        if args.Cancelled then cancelled.Trigger ()
-        else completed.Trigger (args.Result :?> int))
-
     do worker.DoWork.Add(
         fun args -> 
             let rec add num =
                 if worker.CancellationPending then
                     args.Cancel <- true
                 elif num < start + number then
-                    result <- result + (array.Item num)
-                    add (num - 1)
+                    result <- result + (array.[num])
+                    add (num + 1)
                 else
                     args.Result <- result    
             add start  
         )
-    
+
+    do worker.RunWorkerCompleted.Add(fun args ->
+        if args.Cancelled then cancelled.Trigger ()
+        elif args.Error <> null then error.Trigger args.Error
+        else completed.Trigger (args.Result :?> int))
+
+    do worker.ProgressChanged.Add(fun args ->
+        progress.Trigger (args.ProgressPercentage, (args.UserState :?> 'a)))
+
     member x.WorkerCompleted = completed.Publish
     member x.WorkerCancelled = cancelled.Publish
-    
+    member x.WorkerError = error.Publish
+    member x.ProgressChanged = progress.Publish
+
     member x.RunWorkerAsync() = worker.RunWorkerAsync()
     member x.CancelAsync() = worker.CancelAsync()
  
-let mas = [for x in 1..1000000 -> 1]
-let list = [for i in 0..99 -> new PlusOneEvet(mas, 1 + i * 10000)]
+let list = [for i in 0..99 -> new PlusOneEvet(1 + i * 10000)]
 for i in 0..99 do
     (list.Item i).RunWorkerAsync()
 
